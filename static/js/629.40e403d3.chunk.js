43b42c17afd7f42a613fcb596000a4ceaeb87044
bogdanluckyman <131710744+bogdanluckyman@users.noreply.github.com>
2024-03-23 20:35:40 +0000

    Deploying to gh-pages from @ bogdanluckyman/Campers@2adc618125518adf8e2ad137a041d5f48d5aaeaa 🚀

---
+++ static/js/629.40e403d3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcampers=self.webpackChunkcampers||[]).push([[629],{629:function(t,L,e){e.d(L,{mH7:function(){return r}});var n=e(2119);function r(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",strokeWidth:"2",d:"M5,6 L1,4.5 L1,18.443038 L12,23 L23,18.443038 L23,4 L19,6 M5,16 L5,2 L12,5 L19,2 L19,16 L12,19 L5,16 Z M11.95,5 L11.95,19"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=629.40e403d3.chunk.js.map
