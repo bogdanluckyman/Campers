96e971867a26d05a4c511b603b700aecbdd7290c
Bogdan <bogdantisenko06@gmail.com>
2024-03-23 05:29:26 +0200

    homepage

--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -1,6 +1,24 @@
 import styled from 'styled-components';
 
 export const Box = styled.div`
-  height: 300px;
-  width: 300px;
+  width: 480px;
+  height: 400px;
+  overflow: hidden;
+  border-radius: 10px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+export const Container = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  padding: 15px;
+  margin: 0;
+`;
+
+export const CardContainer = styled.li`
+  width: 480px;
 `;
