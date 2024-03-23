fa8e6e6fc1883e072bb7ede1396ae076cc646e62
Bogdan <bogdantisenko06@gmail.com>
2024-03-23 03:40:16 +0200

    start

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import { Box, BoxNav, StyledLink } from './Header.styled';
+
+const Header = () => {
+  return (
+    <Box>
+      <BoxNav>
+        <StyledLink to="/">Home</StyledLink>
+        <StyledLink to="/catalog">Catalog</StyledLink>
+        <StyledLink to="/favorites">Favorite</StyledLink>
+      </BoxNav>
+    </Box>
+  );
+};
+
+export default Header;
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+import colors from './colors';
+import 'modern-normalize';
+
+const GlobalStyles = createGlobalStyle`
+body {
+font-family: 'Roboto', sans-serif; 
+background-color: ${colors.white};
+margin: 0; 
+padding: 0; 
+}
+
+a {  
+text-decoration: none; 
+}
+
+a:hover {
+text-decoration: underline; 
+}
+
+li {
+    list-style: none;
+}
+
+img {
+max-width: 100%;
+height: auto; 
+}
+`;
+
+export default GlobalStyles;
